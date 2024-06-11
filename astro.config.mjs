0f490bbcd8e2ecdcd7296e2ce78a4b7ac08ebe39
Ihor <zimzigor@gmail.com>
2024-06-11 06:10:37 +0300

    gg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
+//import cloudflare from '@astrojs/cloudflare';
 import vercel from '@astrojs/vercel/serverless';
-import { inject } from '@vercel/analytics';
-inject();
+//import { inject } from '@vercel/analytics';
+//inject();
+import zeabur from '@zeabur/astro-adapter/serverless';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
-    adapter: cloudflare(),
+    adapter: zeabur(),
+    //adapter: cloudflare(),
     //outDir: './dist'
     //output: 'server',
     //adapter: vercel(),
